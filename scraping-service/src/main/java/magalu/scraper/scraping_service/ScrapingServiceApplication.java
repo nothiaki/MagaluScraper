9893e32871d5cd908302558ee354ac4c7ce6695e
nothiaki <celsohenrique367@gmail.com>
2025-05-30 17:37:54 -0300

    feat(scraping-service): init

---
+++ scraping-service/src/main/java/magalu/scraper/scraping_service/ScrapingServiceApplication.java
@@ -0,0 +1,9 @@
+package magalu.scraper.scraping_service;
+
+public class ScrapingServiceApplication {
+
+	public static void main(String[] args) {
+    System.out.println("App started");
+	}
+
+}
---
+++ scraping-service/src/test/java/magalu/scraper/scraping_service/ScrapingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package magalu.scraper.scraping_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
